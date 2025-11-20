c1f11d056731460fb6cd0e410904ca0fa7b7dc7f
Rékási József <oktatas.rekasi@gmail.com>
2025-11-20 19:59:19 +0100

    Eloquent orm kezdés

---
+++ app/Models/MenuItem.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MenuItem extends Model {
+
+    protected $table = "menuitems";
+    public $timestamps = false;
+
+    // protected $fillable = [
+
+    //     "name", "category_id", "price"
+    // ];
+
+    public function category() {
+
+        return $this->belongsTo( Category::class );
+    }
+}
